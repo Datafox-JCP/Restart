e608d3b605de0d0cb3757218978cb1977d3a2560
Juan Carlos Pazos <datafox@outlook.com>
2021-11-09 08:26:54 -0600

    Animations and transitions

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -11,6 +11,7 @@ struct HomeView: View {
     // MARK: - Propiedades
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
     
     // MARK: -  Body
     var body: some View {
@@ -23,11 +24,17 @@ struct HomeView: View {
                 Image("character-2")
                     .resizable()
                     .scaledToFit()
-                .padding()
+                    .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(Animation
+                                .easeInOut(duration: 4)
+                                .repeatForever()
+                               , value: isAnimating
+                    )
             } // ZStack
             
             // MARK: - Center
-            Text("The tome taht leads to mastery is dependent on the intensity of our focus")
+            Text("The road that leads to mastery is dependent on the intensity of your focus")
                 .font(.title3)
                 .fontWeight(.light)
                 .foregroundColor(.secondary)
@@ -38,7 +45,9 @@ struct HomeView: View {
             Spacer()
             
             Button(action: {
-                isOnboardingViewActive = true
+                withAnimation {
+                    isOnboardingViewActive = true
+                }
             }) {
                 Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                     .imageScale(.large)
@@ -51,6 +60,11 @@ struct HomeView: View {
             .buttonBorderShape(.capsule)
             .controlSize(.large)
         } // VStack
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                isAnimating = true
+            }
+        })
     }
 }
 
--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -14,6 +14,10 @@ struct OnboardingView: View {
     
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffeset: CGFloat = 0
+    @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Never give in"
     
     // MARK: -  Body
     var body: some View {
@@ -26,33 +30,79 @@ struct OnboardingView: View {
                 Spacer()
                 
                 VStack(spacing: 0) {
-                    Text("Never give in")
-                        .font(.system(size: 50))
+                    Text(textTitle)
+                        .font(.system(size: 50, design: .rounded))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(textTitle)
                     
                     Text("""
                     Never give in. Never give in. Never give in. Never, never, never, never --in nothing, great or small, large or petty-- never give in, except to convictions of honour and good sense. Never yield to force. Never yield to the apparently overwhelming might of the enemy.
                     """)
-                        .font(.title3)
+                        .font(.system(.body, design: .rounded))
                         .fontWeight(.light)
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
                         .padding(.horizontal, 10)
                 }
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+                
                 // MARK: - CENTER
                 
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
+                    
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .gesture(
+                            DragGesture()
+                                .onChanged{ gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTitle = "NEVER GIVE IN"
+                                        }
+                                    }
+                                }
+                                .onEnded{ gesture in
+                                    imageOffset = .zero
+                                    
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Never give in"
+                                    }
+                                }
+                        ) // Gesture
                 } // ZStack
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom
+                )
+                
+                Spacer()
                 
                 // MARK: - FOOTER
                 ZStack {
                     // PARTS OF THE CUSTOM BUTTON
-                
+                    
                     // 1. BACKGROUND (STATIC)
                     Capsule()
                         .fill(Color.white.opacity(0.2))
@@ -98,21 +148,30 @@ struct OnboardingView: View {
                                     }
                                 }
                                 .onEnded { _  in
-                                    if buttonOffeset > buttonWidth / 2 {
-                                        buttonOffeset = buttonWidth - 80
-                                        isOnboardingViewActive = false
-                                    } else {
-                                        buttonOffeset = 0
+                                    withAnimation(Animation.easeOut(duration: 0.4)) {
+                                        if buttonOffeset > buttonWidth / 2 {
+                                            buttonOffeset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        } else {
+                                            buttonOffeset = 0
+                                        }
                                     }
                                 }
                         ) // Gesture
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                         Spacer()
                     } // HStack
                 } // Footer
                 .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
             } // VStack
         } // ZStack
+        .onAppear(perform: {
+            isAnimating = true
+        })
     }
 }
 
